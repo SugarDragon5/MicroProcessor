6a502747aca75ef722ae8327fec93ed3d35f6b4c
Ryugo SATO <sugardragon2nd@gmail.com>
2023-11-02 11:31:48 +0900

    fix: typoの修正とコメントの追加

--- modules/pc.v
+++ modules/pc.v
@@ -2,22 +2,27 @@ module PC (
     clk,npc,pc
 );
     input wire clk;
-    input wire [31:0] npc;
-    output reg [31:0] pc;
+    input wire [31:0] npc;  //次クロックのプログラムカウンタ
+    output reg [31:0] pc;   //現在のプログラムカウンタ
     always @(posedge clk) begin
         pc <= npc;
     end
 endmodule
 
 module NPCGenerator (
-    pc,alucode,imm,reg1dat,result,npc
+    pc,         //現在のプログラムカウンタ
+    alucode,    //ALUコード
+    imm,        //即値
+    reg1dat,    //レジスタ1のデータ
+    br_taken,   //分岐の有無
+    npc         //次クロックのプログラムカウンタ
 );
     input wire [31:0] pc;
     input wire [5:0] alucode;
     input wire [31:0] imm;
     input wire [31:0] reg1dat;
     input wire [31:0] br_taken;
-    output reg [31:0] npc;
+    output wire [31:0] npc;
     
     function [31:0] NPC;
         input [31:0] pc;
@@ -33,5 +38,5 @@ module NPCGenerator (
             NPC=pc+4;
         end
     endfunction
-    assign npc=NPC(pc,imm,reg1dat,result);
+    assign npc=NPC(pc,alucode,imm,reg1dat,br_taken);
 endmodule
