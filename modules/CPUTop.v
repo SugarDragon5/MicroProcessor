0c2537e2e4b4b961fba4fc37d97de0b1ef3fccd3
Ryugo SATO <sugardragon5@g.ecc.u-tokyo.ac.jp>
2023-11-13 22:31:09 +0900

    update: IFステージ・IDステージ

--- modules/CPUTop.v
+++ modules/CPUTop.v
@@ -3,20 +3,77 @@ module CPUTop (
     input wire nrst,
     output wire uart_tx
 );
-    //リセット信号
+//クロック・リセット信号
     wire clk;
     wire rst;
     assign clk=sysclk;
     assign rst = ~nrst;
-    //ステージ変数
-    reg [3:0] stage;
-    //プログラムカウンタ変数
-    reg [31:0] pc;
-    wire [31:0] npc;
-    wire [31:0] regdata1,regdata2;
-    wire [31:0] imm;
-    wire br_taken;
-    wire [5:0] alucode;
+
+//IFステージ
+    reg [31:0] pc_IF;
+    reg [31:0] iword_IF;
+
+//IDステージ
+    reg [31:0] pc_ID;
+    //デコーダ入出力
+    reg [31:0] iword_ID:
+    wire [4:0] srcreg1_num_ID,srcreg2_num_ID,dstreg_num_ID;
+    wire [31:0] imm_ID;
+    wire [5:0] alucode_ID;
+    wire [1:0] aluop1_type_ID,aluop2_type_ID;
+    wire reg_we_ID,is_load_ID,is_store_ID,is_halt_ID;
+    wire [1:0] ram_read_size_ID,ram_write_size_ID;
+    wire ram_read_signed_ID;
+    //レジスタ読み込み
+    wire [31:0] regdata1_ID,regdata2_ID;
+    //オペランドスイッチャー
+    wire [31:0] oprl_ID,oprr_ID;
+
+
+    ROM rom1(
+        .clk(clk),
+        .r_addr(pc_IF[15:2]),
+        .r_data(iword_IF)
+    );
+    decoder decoder1(
+        .ir(iword_ID),
+        .srcreg1_num(srcreg1_num_ID),
+        .srcreg2_num(srcreg2_num_ID),
+        .dstreg_num(dstreg_num_ID),
+        .imm(imm_ID),
+        .alucode(alucode_ID),
+        .aluop1_type(aluop1_type_ID),
+        .aluop2_type(aluop2_type_ID),
+        .reg_we(reg_we_state_ID),
+        .is_load(is_load_ID),
+        .is_store(is_store_ID),
+        .is_halt(is_halt_ID),
+        .ram_read_size(ram_read_size_ID),
+        .ram_write_size(ram_write_size_ID),
+        .ram_read_signed(ram_read_signed_ID)
+    );
+    RegisterFile register1(
+        .clk(clk),
+        //読み込み : IDステージ
+        .srcreg1_num(srcreg1_num_ID),
+        .srcreg2_num(srcreg2_num_ID),
+        .regdata1(regdata1_ID),
+        .regdata2(regdata2_ID),
+        //書き込み : RWステージ
+        .dstreg_num(dstreg_num),
+        .write_value(reg_write_value),
+        .reg_we(reg_we)
+    );
+    OperandSwitcher ops1(
+        .aluop1_type(aluop1_type_ID),
+        .aluop2_type(aluop2_type_ID),
+        .pc(pc_ID),
+        .regdata1(regdata1_ID),
+        .regdata2(regdata2_ID),
+        .imm(imm_ID),
+        .oprl(oprl_ID),
+        .oprr(oprr_ID)
+    );
     NPCGenerator NPCGen1(
         .pc(pc),
         .alucode(alucode),
@@ -25,67 +82,6 @@ module CPUTop (
         .br_taken(br_taken),
         .npc(npc)
     );
-    //ROM
-    wire [31:0] iword;
-    ROM rom1(
-        .clk(clk),
-        .r_addr(pc[15:2]),
-        .r_data(iword)
-    );
-    //命令デコーダ
-    reg decoder_reset;
-    wire [4:0] srcreg1_num,srcreg2_num,dstreg_num;
-    wire [1:0] aluop1_type,aluop2_type;
-    wire reg_we_state,is_load,is_store,is_halt;
-    wire [1:0] ram_read_size,ram_write_size;
-    wire ram_read_signed;
-    decoder decoder1(
-        .clk(clk),
-        .rst(rst|decoder_reset),
-        .ir(iword),
-        .srcreg1_num(srcreg1_num),
-        .srcreg2_num(srcreg2_num),
-        .dstreg_num(dstreg_num),
-        .imm(imm),
-        .alucode(alucode),
-        .aluop1_type(aluop1_type),
-        .aluop2_type(aluop2_type),
-        .reg_we(reg_we_state),
-        .is_load(is_load),
-        .is_store(is_store),
-        .is_halt(is_halt),
-        .ram_read_size(ram_read_size),
-        .ram_write_size(ram_write_size),
-        .ram_read_signed(ram_read_signed)
-    );
-    //レジスタファイル
-    wire [31:0] reg_write_value;
-    reg reg_we;
-    RegisterFile register1(
-        .clk(clk),
-        .srcreg1_num(srcreg1_num),
-        .srcreg2_num(srcreg2_num),
-        .dstreg_num(dstreg_num),
-        .write_value(reg_write_value),
-        .reg_we(reg_we),
-        .regdata1(regdata1),
-        .regdata2(regdata2)
-    );
-    //オペランドスイッチャー
-    wire [31:0] oprl,oprr;
-    OperandSwitcher ops1(
-        .aluop1_type(aluop1_type),
-        .aluop2_type(aluop2_type),
-        .pc(pc),
-        .regdata1(regdata1),
-        .regdata2(regdata2),
-        .imm(imm),
-        .oprl(oprl),
-        .oprr(oprr)
-    );
-    //ALU
-    reg alu_reset;
-    wire [31:0] alu_result;
     alu alu1(
         .clk(clk),
         .rst(rst|alu_reset),
@@ -143,7 +139,6 @@ module CPUTop (
     assign uart_we = ((mem_address == `UART_ADDR) && (is_store == `ENABLE)) ? 1'b1 : 1'b0;  // シリアル通信用アドレスへのストア命令実行時に送信開始信号をアサート
     assign uart_tx = uart_OUT_data;  // シリアル通信モジュールの出力はFPGA外部へと出力
 
-
     //for debug
     `ifdef COREMARK_UART_DEBUG
         always @(posedge uart_we) begin
@@ -163,63 +158,33 @@ module CPUTop (
     //ステージ遷移
     always @(posedge clk) begin
         if(rst)begin
-            stage<=`IF_STAGE;
-            pc<='h8000;
+            pc_IF<='h8000;
+            pc_ID<=0;
+            pc_EX<=0;
+            pc_MA<=0;
+            pc_RW<=0;
         end else begin
-            case(stage)
-                //命令フェッチクロック
-                `IF_STAGE:begin
-                    //PCの更新→デコーダが走る
-                    stage <= `RR_STAGE;
-                    decoder_reset<=1;
-                end
-                //レジスタ・データ準備クロック
-                `RR_STAGE:begin
-                    //デコーダが計算した値をもとに、レジスタからデータを読み出す
-                    stage <= `EX_STAGE;
-                    decoder_reset<=0;
-                    alu_reset<=1;
-                    `ifdef COREMARK_TRACE
-                        $write("0x%4x: 0x%8x",pc[15:0],iword);
-                    `endif
-                end
-                //演算クロック
-                `EX_STAGE:begin
-                    //デコーダ・レジスタの値をもとに、ALUが演算を行う
-                    stage <= `MA_STAGE;
-                    alu_reset<=0;
-                    if((mem_address == `UART_ADDR) && (is_store == `ENABLE))ram_we<=0;
-                    else ram_we<=is_store;
-                end
-                //RAM書き込みクロック
-                `MA_STAGE:begin
-                    //次クロックにレジスタ書き込みを行うかを書き込む
-                    stage <= `RW_STAGE;
-                    ram_we<=0;
-                    reg_we<=reg_we_state;
-                end
-                //レジスタ書き込みクロック
-                `RW_STAGE:begin
-                    stage <= `IF_STAGE;
-                    reg_we<=0;
-                    pc <= npc;
-                    `ifdef COREMARK_TRACE
-                        if(reg_we)begin
-                            $write(" # x%02d = 0x%8x",dstreg_num,reg_write_value);
-                        end else $write(" # (no destination)");
-                        if(is_store)begin
-                            if(ram_write_size==`RAM_MODE_BYTE)$write("; mem[0x%08x] <- 0x%02x",mem_address,mem_write_value[7:0]);
-                            else if(ram_write_size==`RAM_MODE_HALF)$write("; mem[0x%08x] <- 0x%04x",mem_address,mem_write_value[15:0]);
-                            else if(ram_write_size==`RAM_MODE_WORD)$write("; mem[0x%08x] <- 0x%08x",mem_address,mem_write_value);
-                        end else if(is_load)begin
-                            if(ram_read_size==`RAM_MODE_BYTE)$write(";            0x%02x <- mem[0x%08x]",mem_load_value[7:0],mem_address);
-                            if(ram_read_size==`RAM_MODE_HALF)$write(";          0x%04x <- mem[0x%08x]",mem_load_value[15:0],mem_address);
-                            if(ram_read_size==`RAM_MODE_WORD)$write(";      0x%08x <- mem[0x%08x]",mem_load_value,mem_address);
-                        end
-                        $write("\n");
-                    `endif
-                end
-            endcase
+            //IFステージ
+            if(分岐あり)pc_IF<=分岐先;
+            else pc_IF<=pc_IF+4;
+            //IDステージ
+            if(分岐あり)begin
+                //分岐予測に失敗しているのでリセット
+                pc_ID<=0;
+                iword_ID<=0;
+            end else begin
+                pc_ID<=pc_IF;
+                iword_ID<=iword_IF;
+            end
+            //EXステージ
+            if(分岐あり)begin
+                //分岐予測に失敗しているのでリセット
+                pc_EX<=0;
+                iword_EX<=0;
+            end else begin
+                pc_EX<=pc_ID;
+                iword_EX<=iword_ID;
+            end
         end
         
     end
